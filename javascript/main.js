a599dbecd825c43b22be48d1548c781cd332e3eb
KgothatsoN <kg.otha@hotmail.com>
2021-03-02 16:44:09 +0000

    Add fetch image and appropriate sizing css image

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,7 @@ const drinkImage = document.getElementById("image");
 const glassType = document.getElementById("glass-type");
 const drinkName = document.getElementById("drink-name");
 const ingredient = document.getElementById("ingredient-1");
+const instructions = document.getElementById("instructions");
 
 button.addEventListener("click", (e) => {
     e.preventDefault();
@@ -15,7 +16,10 @@ const getData = () => {
     fetch(url)
     .then(res => res.json())
     .then(data =>{
+        drinkImage.src = data.drinks[0].strDrinkThumb
         drinkName.innerHTML = data.drinks[0].strDrink
         glassType.innerHTML = data.drinks[0].strGlass
+        instructions.innerHTML = data.drinks[0].strInstructions
+        console.log(data)
     }); 
 }
